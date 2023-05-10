6ea97eaedb00c5856f5f05b3169538822fa709de
Louis Manceron <louis.manceron@gmail.com>
2023-05-10 16:55:10 +0200

    correction

--- jour_julien.R
+++
@@ -1,8 +0,0 @@
-
-jour_julien <- function (df){
-  df$date_de_session <- as.Date(df$date_de_session, format = "%d/%m/%Y") # ça marche 
-  df$jour_julien <- format(df$date_de_session, "%j")
-  return (df)
-}
-
-#df_test <- jour_julien(sf_obs)
--- pre_traitement.R
+++
@@ -1,2 +0,0 @@
-
-df_test <- df_test[!duplicated(paste(df_test$lat,df_test$long,df_test$date_de_session)),]
